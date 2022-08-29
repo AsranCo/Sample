87d9a30bf74fd5f25000b73a218f85c9ef60109b
info@asran.ir <info@asran.ir>
2022-08-29 16:45:52 +0430

    quera task

---
+++ Quera/147635.py
@@ -0,0 +1,11 @@
+def ColHeat(num):
+    x = input().split(maxsplit=num)
+
+    for i in range(num):
+        if int(x[i]) > 15:
+            print("cooler")
+        else:
+            print("heater")
+
+
+ColHeat(int(input()))
