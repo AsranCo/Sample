e126f1969a543a0de3e64fa220cf63645d8f1a5a
info@asran.ir <info@asran.ir>
2022-09-03 17:48:54 +0430

    quera college

---
+++ college.py
@@ -0,0 +1,49 @@
+###############################################################
+# words = [['s', 'a', 'l', 'a', 'm'], ['k', 'h', 'o', 'o', 'b', 'i', '?']]
+#
+# # for word in words:
+# for char in words:
+#     print(char, end='')
+# print()
+
+###############################################################
+# def Zarb(n):
+#     m = 1
+#     while n >= m:
+#         for i in range(1, n + 1):
+#             print(i * m, end=' ')
+#         print()
+#         m = m + 1
+#
+#
+# Zarb(int(input()))
+
+###############################################################
+# def Mazrab():
+#     p, d = map(int, input().split())
+#     while True:
+#         if 0 <= d % p <= p / 2:
+#             print(d)
+#             break
+#         else:
+#             d += d
+#             continue
+#
+#
+# Mazrab()
+
+###############################################################
+
+
+# s = list()
+# while (s2 := input()) != "test":
+#     s.append(s2)
+#     print(s)
+
+# import math
+#
+# n = 9
+# k = 4
+# result = math.comb(n, k)
+# print('comb(n, k) :', result)
+###############################################################
