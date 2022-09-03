c817aaf1809385cc405fceccf2062dcb169929e1
info@asran.ir <info@asran.ir>
2022-09-03 23:01:50 +0430

    quera college

--- college.py
+++ college.py
@@ -47,6 +47,14 @@
 # result = math.comb(n, k)
 # print('comb(n, k) :', result)
 ###############################################################
+# import math
+# x = int(input())
+# y = math.ceil(math.pow(x, 5/3) + math.tan(math.radians(x)))
+# z = math.floor(math.pow(math.pi, (2 + math.atan(math.pow(math.sin(math.radians(x)), 2)))))
+# print(math.gcd(y, z))
+
+###############################################################
+
 # from datetime import date, datetime
 #
 #
