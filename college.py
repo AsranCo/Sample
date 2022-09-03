a4d451ea21fa3133f2df847c8d6c9534f816929f
info@asran.ir <info@asran.ir>
2022-09-03 22:59:43 +0430

    quera college

--- college.py
+++ college.py
@@ -47,3 +47,70 @@
 # result = math.comb(n, k)
 # print('comb(n, k) :', result)
 ###############################################################
+# from datetime import date, datetime
+#
+#
+# def day_calculator(n):
+#     sajjad_bdate = date(1999, 1, 14)
+#     birthday = n.split("-")
+#     bi = date(int(birthday[0]), int(birthday[1]), int(birthday[2]))
+#     age = (bi - sajjad_bdate).days
+#     if int(age) > 0:
+#         return (age)
+#     else:
+#         return ("Not yet born")
+###############################################################
+# def karim(n):
+#     x = []
+#     l = []
+#     for f in range(n):
+#         x.append(str(input()))
+#     for i in range(n):
+#         l.append(len(set(x[i])))
+#     l.sort()
+#     print(l[-1])
+#
+#
+# karim(int(input()))
+
+
+###############################################################
+# rang = int(input())
+# ramz = input()
+#
+# li = []
+# c = 0
+# for f in range(rang):
+#     li.append(str(input()))
+# for i in range(len(ramz)):
+#     if li[i].find(ramz[i]) > 4:
+#         print(9 - li[i].find(ramz[i]))
+#         c = c + (9 - li[i].find(ramz[i]))
+#     else:
+#         print( li[i].find(ramz[i]))
+#         c = c + li[i].find(ramz[i])
+# print(c)
+#
+###############################################################
+# print(str("{:b}".format(int(input()))).count("1"))
+###############################################################
+
+# import re
+#
+#
+# def validate_email(email):
+#     regex = r'\b[A-Za-z0-9._]+@[A-Za-z0-9]+\.[A-Za-z]{3}\b'
+#     if (re.fullmatch(regex, email)):
+#         return True
+#     else:
+#         return False
+#
+#
+# def validate_phone(number):
+#     regex = r'^(?:09|\+989|00989)?(\d{9})$'
+#     if (re.fullmatch(regex, number)):
+#         return True
+#     else:
+#         return False
+###############################################################
+
