21b2888356de4ff7d9fb198fe78603d4c593d0e9
info@asran.ir <info@asran.ir>
2022-09-12 23:23:15 +0430

    Quera College

---
+++ Quera-College/26_research.py
@@ -0,0 +1,22 @@
+##جستوجوگر
+
+import os
+
+
+def explore(ttype, address):
+    file_dict = {}
+    for root, dirs, files in os.walk(address):
+        count = 1
+        for file in files:
+            if file.endswith(ttype.lower()):
+                path = os.path.join(root)
+                if path in file_dict:
+                    count = file_dict[path] + 1
+                    file_dict[os.path.join(root)] = count
+                else:
+                    file_dict[os.path.join(root)] = count
+
+    return (file_dict)
+
+
+explore("csv", "/home/pc/snap")
---
+++ Quera-College/27_organize.py
@@ -0,0 +1,12 @@
+##سازماندهی رسانه
+
+import os
+import sys
+
+
+def media(src, dst):
+    print(src)
+    print(dst)
+
+
+media(sys.argv[1], sys.argv[2])
