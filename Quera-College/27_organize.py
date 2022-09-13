cb9b4bdf0686be4b0fdcdd8f2ab0afe785f5d8cb
info@asran.ir <info@asran.ir>
2022-09-13 15:57:54 +0430

    Quera College

--- Quera-College/27_organize.py
+++ Quera-College/27_organize.py
@@ -1,12 +1,39 @@
 ##سازماندهی رسانه
-
+import datetime
 import os
 import sys
 
 
 def media(src, dst):
-    print(src)
-    print(dst)
+    print("Last modified: %s" % (datetime.datetime.fromtimestamp(os.path.getctime(src))).strftime("%Y"))
 
+    image = (".jpg", ".jpeg", ".png")
+    video = (".mp4", ".avi", ".3gp", ".mpeg", ".mkv", ".wmv", ".mov")
+
+    for folder, subfolders, filename in os.walk(src):
+        print(filename)
+
+        if any([filename.lower().endswith(tuple(image)) for filename in filename]):
+            print("Last modified: %s" % (datetime.datetime.fromtimestamp(os.path.getctime())).strftime("%Y"))
+
+
+#     file_dict = {}
+#     for root, dirs, files in os.walk(src):
+#         for file in files:
+#             if file.lower().endswith(ttype.lower()):
+#                 path = os.path.join(root)
+#                 if path in file_dict:
+#                     count = file_dict[path] + 1
+#                     file_dict[os.path.join(root)] = count
+#                 else:
+#                     file_dict[os.path.join(root)] = count
+#
+#
+# try:
+#     os.mkdir(dst)
+# except FileExistsError:
+#     pass
+#
+#     return (file_dict)
 
 media(sys.argv[1], sys.argv[2])
