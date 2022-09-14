6d25a1cc8aa2fa81d3bbd43c4c7d6b99534cf81e
info@asran.ir <info@asran.ir>
2022-09-14 17:27:32 +0430

    Quera College

--- Quera-College/F5_organize.py
+++ Quera-College/F5_organize.py
@@ -5,35 +5,28 @@ import sys
 
 
 def media(src, dst):
-    print("Last modified: %s" % (datetime.datetime.fromtimestamp(os.path.getctime(src))).strftime("%Y"))
-
     image = (".jpg", ".jpeg", ".png")
     video = (".mp4", ".avi", ".3gp", ".mpeg", ".mkv", ".wmv", ".mov")
 
     for folder, subfolders, filename in os.walk(src):
-        print(filename)
+        for f in filename:
+            if any([f.lower().endswith(tuple(video))]):
+                filepath_video = (f"{folder}" + f"/{f}")
+                modef_time = (datetime.datetime.fromtimestamp(os.path.getctime(filepath_video))).strftime("%Y")
+                try:
+                    os.makedirs(f"{dst}/{modef_time}/photos")
+                except FileExistsError:
+                    pass
 
-        if any([filename.lower().endswith(tuple(image)) for filename in filename]):
-            print("Last modified: %s" % (datetime.datetime.fromtimestamp(os.path.getctime(src))).strftime("%Y"))
+            if any([f.lower().endswith(tuple(image))]):
+                filepath_image = (f"{folder}" + f"/{f}")
+                modef_time = (datetime.datetime.fromtimestamp(os.path.getctime(filepath_image))).strftime("%Y")
+                try:
+                    print(dst)
+                    os.makedirs(f"{dst}/{modef_time}/videos")
+                except FileExistsError:
+                    pass
 
 
-#     file_dict = {}
-#     for root, dirs, files in os.walk(src):
-#         for file in files:
-#             if file.lower().endswith(ttype.lower()):
-#                 path = os.path.join(root)
-#                 if path in file_dict:
-#                     count = file_dict[path] + 1
-#                     file_dict[os.path.join(root)] = count
-#                 else:
-#                     file_dict[os.path.join(root)] = count
-#
-#
-# try:
-#     os.mkdir(dst)
-# except FileExistsError:
-#     pass
-#
-#     return (file_dict)
-
-media(sys.argv[1], sys.argv[2])
+media("./file/src", "./file/dst")
+# media(sys.argv[1], sys.argv[2])
