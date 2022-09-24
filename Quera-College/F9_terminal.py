806bbe97d71c437b7bf7aee696255a57059bfb8b
info@asran.ir <info@asran.ir>
2022-09-25 01:48:12 +0330

    Quera College

--- Quera-College/F9_terminal.py
+++ Quera-College/F9_terminal.py
@@ -1,56 +1,58 @@
 import pygame
 
-from math import pi
-
-
-draw line 100 150 200 150
-draw line 150 100 150 200
-change color 255 0 0
-change size 2
-draw circle 150 150 50
-end drawing
-
-
 pygame.init()
+screen = pygame.display.set_mode((300, 300))
+pygame.display.set_caption("test window")
 
-screen = pygame.display.set_mode((800, 600))
-
-red = (252.2, 0, 0)
-
-blue = (0, 0, 255)
-
-amber = (100, 75, 0)
-
-aqua = (0, 100, 100)
-
-bittersweet = (100, 44, 37)
-
-green = (0, 255, 0)
-
-aero_blue = (75, 91, 84)
-
-gray = (128, 128, 128)
-
-screen.fill(gray)
-
-pygame.draw.line(screen, red, (300.4, 300), (600, 400), 10)
-
-pygame.draw.circle(screen, amber, (200, 200), 20, 6)
-
-pygame.draw.rect(screen, blue, (400, 400, 30, 80), 20)
-
-pygame.draw.ellipse(screen, aqua, (450, 450, 100, 50), 10)
-
-pygame.draw.polygon(screen, bittersweet, [(700, 500), (600, 500), (550, 450), (650, 440)], 10)
-
-pygame.draw.lines(screen, green, True, [(700, 200), (600, 200), (550, 150), (650, 140)], 20)
-
-pygame.draw.lines(screen, green, False, [(700, 350), (600, 350), (550, 300), (650, 290)], 30)
-
-pygame.draw.arc(screen, aero_blue,  (210, 75, 150, 125), 3*pi/2, 2*pi, 25)
-
+wi = (255, 255, 255)
+size = 1
+color = (0, 0, 0)
+screen.fill(wi)
 pygame.display.update()
-
 while True:
+    intput = input()
 
-    pygame.event.pump()
+    if intput == "end drawing":
+        pygame.event.pump()
+        pygame.image.save(screen, 'draw.png')
+        break
+
+    elif intput.find("size") != -1:
+        size = int(intput.replace("change size ", ""))
+
+    elif intput.find("color") != -1:
+        x = intput.replace("change color ", "").replace(" ", ",")
+        for i in range(len(x.split(","))):
+            y = list(color)
+            y[i] = int(x.split(",")[i])
+            color = tuple(y)
+
+    elif intput.find("line") != -1:
+        x = intput.replace("draw line ", "").replace(" ", ",")
+        for i in range(len(x.split(","))):
+            pygame.draw.line(screen, color, (int(x.split(",")[0]), int(x.split(",")[1])),
+                             (int(x.split(",")[2]), int(x.split(",")[3])), size)
+        pygame.display.update()
+
+    elif intput.find("circle") != -1:
+        x = intput.replace("draw circle ", "").replace(" ", ",")
+        for i in range(len(x.split(","))):
+            pygame.draw.circle(screen, color, (float(x.split(",")[0]), float(x.split(",")[1])), (int(x.split(",")[2])),
+                               size)
+        pygame.display.update()
+
+    elif intput.find("polygon") != -1:
+        x = intput.replace("draw polygon ", "").replace(" ", ",")
+        for i in range(len(x.split(","))):
+            pygame.draw.polygon(screen, color, [(float(x.split(",")[0]), float(x.split(",")[1])),
+                                                (float(x.split(",")[2]), float(x.split(",")[3])),
+                                                (float(x.split(",")[4]), float(x.split(",")[5])),
+                                                (float(x.split(",")[6]), float(x.split(",")[7]))], size)
+        pygame.display.update()
+
+# draw line 100 150 200 150
+# draw line 150 100 150 200
+# change color 255 0 0
+# change size 2
+# draw circle 150 150 50
+# end drawing
